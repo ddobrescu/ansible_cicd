c251441ad7089a2900b71aa3120ea892da8de182
ddobrescu <47787239+ddobrescu@users.noreply.github.com>
2023-07-17 00:00:51 -0400

    Create active_ansible.ps1

---
+++ active_ansible.ps1
@@ -0,0 +1,6 @@
+$url = "https://github.com/AlbanAndrieu/ansible-windows/blob/master/files/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
